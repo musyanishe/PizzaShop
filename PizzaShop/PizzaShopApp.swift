92e66f803e43ff8b6a9530d48e046bf1808ca846
Evgeniia Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-08-20 15:43:18 +0300

    Initial Commit

---
+++ PizzaShop/PizzaShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaShopApp.swift
+//  PizzaShop
+//
+//  Created by Евгения Шевцова on 20.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct PizzaShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
